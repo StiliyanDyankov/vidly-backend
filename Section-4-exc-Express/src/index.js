4748c28c8c31d832bdb310db9b3bc8a4a1b66a94
Stiliyan Dyankov <stiliandiankov0704@gmail.com>
2023-01-13 23:04:25 +0200

    Setting up basic rest api - courses

--- Section-4-exc-Express/src/index.js
+++ Section-4-exc-Express/src/index.js
@@ -1,15 +1,30 @@
 const express = require("express");
 const app = express();
 
+const courses = [
+    {id:1, name: 'course1'},
+    {id:2, name: 'course2'},
+    {id:3, name: 'course3'},
+    {id:4, name: 'course4'},
+];
+
 app.get('/', (req, res)=>{
-    res.send('Hello world');
+    res.send('Hello world!!');
 });
 
-app.get('/api/chisla', (req, res)=>{
-    res.send([1,2,3]);
+app.get('/api/courses', (req, res)=>{
+    res.send(courses);
+});
+
+const port = process.env.PORT || 3000;
+
+app.get('/api/courses/:id', (req, res)=>{
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) res.status(404).send('The course with the given id was not found');
+    res.send(course);
 });
 
 
-app.listen(3000, ()=>{
-    console.log('listening on port 3000...');
+app.listen(port, ()=>{
+    console.log(`listening on port ${port}...`);
 })
