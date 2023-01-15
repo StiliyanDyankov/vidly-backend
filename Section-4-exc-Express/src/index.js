09c2216e31ce1b66e16b98a39021fc5f7cc88038
Stiliyan Dyankov <stiliandiankov0704@gmail.com>
2023-01-15 20:23:02 +0200

    Building basic api for vidly-backend

--- Section-4-exc-Express/src/index.js
+++ Section-4-exc-Express/src/index.js
@@ -24,15 +24,16 @@ app.get("/api/courses", (req, res) => {
 app.get("/api/courses/:id", (req, res) => {
     const course = courses.find((c) => c.id === parseInt(req.params.id));
     if (!course)
-        res.status(404).send("The course with the given id was not found");
+        return res
+            .status(404)
+            .send("The course with the given id was not found");
     res.send(course);
 });
 
 app.post("/api/courses", (req, res) => {
-    const result = schema.validate(req.body);
+    const { error } = validateCourse(req.body);
 
-    if (result.error) return res.status(400).send(result.error);
-
+    if (error) return res.status(400).send(error.details[0].message);
     const course = {
         id: courses.length + 1,
         name: req.body.name,
